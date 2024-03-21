563e49d0e80ae9b6aee9e089de84c5cdc72b5396
Aldiva Wibowo <90434857+Aldbow@users.noreply.github.com>
2024-03-21 12:29:51 +0700

    feat: :dizzy: Fade animation
    Added fade vertical animation when scrolling down. i applied this fade animation for each container within section

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,24 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Fading animation for sections with better performance
+const allSections = document.querySelectorAll(".section"); // Selecting all sections with the 'section' class
+
+const fadingSection = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section__hidden"); // Removing hidden class to trigger fade-in animation
+  observer.unobserve(entry.target); // Unobserving the section once it's visible
+};
+
+// Creating Intersection Observer for fading animation
+const sectionObserver = new IntersectionObserver(fadingSection, {
+  root: null,
+  threshold: 0.12,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section); // Observing each section
+  section.classList.add("section__hidden"); // Adding hidden class initially
+});
